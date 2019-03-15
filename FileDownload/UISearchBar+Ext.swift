d88a039a29652de1429a342e244639fd4d2f1fbf
mail4karthickr@gmail.com <mail4karthickr@gmail.com>
2019-03-15 07:50:22 -0700

    add UISearchBar extension

---
+++ FileDownload/UISearchBar+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+Ext.swift
+//  FileDownload
+//
+//  Created by Karthick Ramasamy on 3/15/19.
+//  Copyright © 2019 Karthick Ramasamy. All rights reserved.
+//
+
+import UIKit
+
+extension UISearchBar {
+    
+}
